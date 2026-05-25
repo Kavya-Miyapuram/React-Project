ebed33e772b1341cd2c5377c6d0ebaeb058c7ca4
Miyapuram Kavya <miyapuramkavya123@gmail.com>
2026-05-25 20:03:39 +0530

    Fix case sensitivity in import path for Merchcont

--- Routes/Merchroutes.js
+++ Routes/Merchroutes.js
@@ -1,6 +1,6 @@
 let express=require("express")
 let router=express.Router()
-let {addmerch,merchlogin}=require("../controller/Merchcont")
+let {addmerch,merchlogin}=require("../Controller/Merchcont")
 router.post("/reg",addmerch)
 router.post("/login",merchlogin)
 module.exports=router
